37f7515bdc73891f50efe1474e6328a1f0058f9b
mangeshsali <mpsali16@gmail.com>
2023-05-08 22:20:21 +0530

    Done

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#auto-type", {
+  strings: ["Web Developer", "Frontend Developer"],
+  startDelay: 50,
+  typeSpeed: 50,
+  backSpeed: 50,
+  smartBackspace: true,
+  loop: true,
+  loopCount: Infinity,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        "regal-blue": "#7c3aed",
+        "black-grey": "#4b5563",
+        "white-shade": "#f5f3ff",
+      },
+    },
+
+    plugins: [],
+  },
+};
